2f39bbf9c6d07718b4fb48245b7a2266c8fed249
RenzoOrtiz <renzo-ortiz@hotmail.com>
2020-08-03 19:05:51 -0300

    "Lucas hoy se trabajo x2"

---
+++ SISTEMA-COMANDA/src/sistema/comanda/SISTEMACOMANDA.java
@@ -0,0 +1,15 @@
+
+package sistema.comanda;
+
+/**
+ *
+ * @author Renzo ortiz
+ */
+public class SISTEMACOMANDA {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
